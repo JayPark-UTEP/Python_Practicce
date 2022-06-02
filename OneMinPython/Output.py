afd44b6d9fbe71c6691b93bed535c8137284d243
Jay Park <88129753+JayPark-UTEP@users.noreply.github.com>
2022-06-02 00:07:05 -0600

    Add files via upload

---
+++ OneMinPython/Output.py
@@ -0,0 +1,35 @@
+import sys
+
+print("Python", "Java", "JavaScript", sep = " vs ")
+print("Python", "Java", "JavaScript", sep = ",", end="?")
+print("What would be more fun?")
+print("------------------------------------------------")
+print("Python", "Java", "JavaScript", file=sys.stdout)
+print("Python", "Java", "JavaScript", file=sys.stderr)
+print("------------------------------------------------")
+
+scores = {"Math":0, "Eng":50, "CS":100}
+for subject, score in scores.items():
+#key and value
+    print(subject, score)
+
+print("------------------------------------------------")
+for subject, score in scores.items():
+    print(subject.ljust(5), str(score).rjust(5), sep=":")
+print("------------------------------------------------")
+for num in range(1, 21):
+    print("Waiting Number : " + str(num).zfill(3))
+print("------------------------------------------------")
+# answer = input("Enter any value: ")
+# print(type(answer)) #any input stored as string
+# print("Entered " + answer
+# )
+
+print("#################################################")
+#Keep empty space, sort to right and get 10 points
+print("{0: > 10}".format(500)) 
+#+ for positive, - for negative
+print("{0: >+10}".format(500))
+print("{0: >+10}".format(-500))
+#sort to left, fill blank space with _
+print("{0: _<+10}".format(500)) 
